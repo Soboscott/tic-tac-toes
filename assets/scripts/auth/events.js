b862443fc9aa7081b391f5b7d894e7966050d815
Soboscott <scott.landry01772@yahoo.com>
2017-02-08 15:45:10 -0500

    reload auth

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,38 @@
+'use strict';
+const config = require ('../config');
+const store =require ('../config');
+//use store
+const signUp = function(data){
+  return $.ajax({
+    url: config.apiOrigin + '/sign-up',
+    method: 'POST',
+    data,
+  });
+
+
+  };
+  const signIn = function(data){
+    return $.ajax({
+      url: config.apiOrigin + '/sign-in',
+      method: 'POST',
+      data,
+    });
+
+
+    };
+    const changePassword = function (data) {
+      return $.ajax({
+        url: `${config.apiOrigin}/change-password/${store.user.id}`,
+        method: 'PATCH',
+        headers: {
+          Authorization: `Token token=${store.user.token}`,
+        },
+        data,
+      });
+    };
+
+module.exports = {
+  signUp,
+  signIn,
+  changePassword
+};
