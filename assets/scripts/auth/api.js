b78da6ed462a9c98aa8b57d2cb49c002022e9732
Soboscott <scott.landry01772@yahoo.com>
2017-02-12 14:05:04 -0500

    upload to get help from cohorts

--- assets/scripts/auth/api.js
+++ assets/scripts/auth/api.js
@@ -61,6 +61,24 @@ const createGame = function () {
   });
 };
 
+// const gameUpdate = function(index, currentPlayer, endGame){
+//   return $.ajax({
+//     url: config.apiOrigin + '/games/' + store.game.id,
+//     method: 'PATCH',
+//     headers: {
+//       Authorization: `Token token=${store.user.token}`,
+//     },
+//     data: {
+//       game: {
+//         cell: {
+//           index: index,
+//           value: currentPlayer,
+//         },
+//       over: endGame,
+//       }
+//     }
+//   });
+// };
 const patchGame = function (id, indexNum, currentPlayer, checkWins){
   return $.ajax({
     url: config.apiOrigin + '/games/' + id,
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,14 +1,67 @@
 'use strict';
+//
+// const success = (data) => {
+//   if (data) { console.log(data); }
+// };
+//
+// const failure = (error) => {
+//   console.error(error);
+// };
+const signUpSuccess =() =>{
+  $('#sign-up1').dropdown('toggle');
+  $('#sign-up').val('');
 
-const success = (data) => {
-  console.log(data);
 };
+const signInSuccess = () => {
+  // $('#sign-in1').dropdown('toggle');
 
-const failure = (error) => {
-  console.error(error);
+  // $('#sign-up').val('');
+  $('#sign-in');
+  $('#sign-up-btb').hide();
+  $('.signinhide').hide();
+  $('#getGame').show();
+  // $('#change-password').show();
+  $('#sign-out').show();
+  $('#reset').show();
+  $('h2').show();
+  $('.changebtn').show();
+
+};
+const signOutSuccess = () => {
+  $('#sign-out').hide();
+  $('#change-password').hide();
+  $('#getGame').hide();
+  $('#reset').hide();
+  $('#wrapper').hide();
+  $('.history').hide().val('');
+  $('#sign-in').show();
+  $('#sign-up').show();
+  $('.showTurn').hide();
+  $('.win').empty();
+  $('.history').empty();
+  $('.signinhide').show();
+  $('#sign-up-btb').show();
+  $('.changebtn').hide();
+
+
+};
+const createGameSuccess = () => {
+  $('#wrapper').show();
+  $('.history').empty();
+  $('.win').show();
+};
+const getGameSuccess = (data) => {
+  // $('.history').show();
+  $('.history').text('You\'ve played ' + data.games.length + ' times!');
+
 };
 
 module.exports = {
-  failure,
-  success,
+  signInSuccess,
+  signOutSuccess,
+  createGameSuccess,
+  getGameSuccess,
+  // success,
+  // failure,
+  signUpSuccess
 };
