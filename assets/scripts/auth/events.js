f9994f66a69be861c9dd5e73f90fe634a717e5b1
Soboscott <scott.landry01772@yahoo.com>
2017-04-02 11:03:24 -0400

    Cleaning code removing comments

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -4,23 +4,22 @@ const getFormFields = require(`../../../lib/get-form-fields`);
 
 const api = require('./api');
 const ui = require('./ui');
-const game = require('./game');
+
 const store = require('../store');
 
 const onSignUp = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
   let data = getFormFields(event.target);
 
   api.signUp(data)
     .then(ui.signUpSuccess)
-    // .then(ui.success)
-
-    // .catch(ui.failure)
-;
+    .catch(ui.emailFailure)
+    ;
 };
+
 const onSignIn = function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
   let data = getFormFields(event.target);
 
@@ -29,90 +28,43 @@ const onSignIn = function (event) {
       store.user = response.user;
       return store.user;
     })
-    .then(ui.signInSuccess);
-
-    // .then(ui.success)
-  // .catch(ui.failure);
-
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+    ;
 };
+
 const onChangePassword = function (event) {
   event.preventDefault();
 
   let data = getFormFields(event.target);
 
   api.changePassword(data)
-    // .then(ui.success)
-    // .catch(ui.failure)
+    .then(ui.success)
+    .catch(ui.failure)
     ;
 };
+
 const onSignOut = function (event) {
   event.preventDefault();
 
   api.signOut()
-    .then(() => {
-      delete store.user;
-      return store;
-    })
-    .then(ui.signOutSuccess)
-    // .catch(ui.failure)
-    ;
+  .then(() => {
+    delete store.user;
+    return store;
+  })
+  .then(ui.success)
+  .catch(ui.failure)
+  ;
 };
-
-
-const onGetGame = function (event) {
-  event.preventDefault();
-
-  api.game()
-
-    .then(ui.getGameSuccess)
-    // .catch(ui.failure)
-    ;
-};
-//
-// $('#sign-in').show();
-
-
-// const onPatchGame = function (event) {
-//   event.preventDefault();
-//   api.patchGame(store.game.id, event.target.id, game.currentPlayer, game.checkWins())
-//   .then(ui.success)
-//   .catch(ui.failure);
-// };
-
-// const onCreateGame = function (event) {
-//   event.preventDefault();
-
-// const gameUpdate = function (event) {
-//   event.preventDefault();
-//   api.createGame()
-//
-//     .then(ui.success)
-//     .catch(ui.failure)
-//     ;
-
 
 const addHandlers = () => {
-  $('#sign-up').show();
-  $('#sign-in').show();
-  $('#wrapper').hide();
-  // $('#wrapper').show();
-
-  $('#getGame').hide();
-  // $('#getGame').show();
-  $('.changebtn').hide();
-  // $('#game-log').hide();
-  $('#sign-out').hide();
-  // $('#reset').hide();
-  // $('#hidePass').hide();
   $('#sign-up').on('submit', onSignUp);
   $('#sign-in').on('submit', onSignIn);
   $('#change-password').on('submit', onChangePassword);
-  $('#sign-out').on('click', onSignOut);
-  $('#getGame').on('click', onGetGame);
-  $('#reset').on('click', game.onCreateGame);
-
+  $('#sign-out').on('submit', onSignOut);
 };
+
 module.exports = {
   addHandlers,
-
+  onSignIn,
 };
